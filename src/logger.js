47ac88377b494c7720b6cfcd56ffa0c06ebba112
Christian Daryl Curay <80902959+christiancuray@users.noreply.github.com>
2025-09-05 14:11:36 -0400

    add pino logger

---
+++ src/logger.js
@@ -0,0 +1,16 @@
+// 'info' as default log level if not specified
+
+const options = {
+  level: process.env.LOG_LEVEL || 'info',
+};
+
+// if the log level is debug, lets make the logs easier to read
+if (options.level === 'debug') {
+  options.transport = {
+    target: 'pino-pretty',
+    options: { colorize: true },
+  };
+}
+
+// export pino logger instance
+module.exports = require('pino')(options);
