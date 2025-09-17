d0cc14e3d086c1360367161ed57f157ae4350336
Christian Daryl Curay <80902959+christiancuray@users.noreply.github.com>
2025-09-17 11:38:01 -0400

    Revert: Undo last 5 commits. pushed to wrong repo

--- assignments/fragments/src/routes/api/get.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res) => {
-  // this is a placeholder response. To get something working, return an empty array.
-  res.status(200).json({
-    status: 'ok',
-    fragments: [],
-    message: 'GET /v1/fragments endpoint is working fine',
-  });
-};
