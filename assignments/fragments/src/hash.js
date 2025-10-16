dc1e494f7104fb76006bcdf30436fefd96bb1ef7
Christian Daryl Curay <80902959+christiancuray@users.noreply.github.com>
2025-10-15 20:08:05 -0400

    feat: add logger

--- assignments/fragments/src/hash.js
+++ assignments/fragments/src/hash.js
@@ -1,14 +1,18 @@
 const crypto = require('crypto');
 const algorithm = 'sha256';
 const secret = process.env.SECRET_HASH || 'secret';
+const logger = require('./logger');
 
 function hash(email) {
-  if (!email || typeof email !== 'string') {
-    throw new Error('Email is required and must be a string');
-  }
   if (email.length === 0) {
+    logger.warn('Email is required and must be a string');
     return crypto.createHash(algorithm).update('').digest('hex');
   }
+  if (!email || typeof email !== 'string') {
+    logger.warn('Email is required and must be a string');
+    throw new Error('Email is required and must be a string');
+  }
+
   const hash = crypto.createHmac(algorithm, secret);
   return hash.update(email).digest('hex');
 }
