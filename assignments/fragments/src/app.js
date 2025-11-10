51ab5604a8e749e6242a01d2c3766d265173d89e
Christian Daryl Curay <80902959+christiancuray@users.noreply.github.com>
2025-11-10 14:36:27 -0500

    feat: made changes to trigger ci

--- assignments/fragments/src/app.js
+++ assignments/fragments/src/app.js
@@ -90,4 +90,4 @@ app.use((err, req, res, next) => {
 });
 module.exports = app;
 
-console.log('testing log to trigger hadolint in ci');
+console.log('testing log to trigger build and push to docker from ci');
