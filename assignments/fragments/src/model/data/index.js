b16c970b721d22381fac7e180352b9445ee62ebc
Christian Daryl Curay <80902959+christiancuray@users.noreply.github.com>
2025-12-02 14:13:20 -0500

    chore: update for db config

--- assignments/fragments/src/model/data/index.js
+++ assignments/fragments/src/model/data/index.js
@@ -1,3 +1,4 @@
-const memory = require('./memory/index');
+// If the environment sets an AWS Region, we will use AWS backend services.
+// ( S3, DynamoDB ); otherwise, we will use in-memory db,
 
-module.exports = memory;
+module.exports = process.env.AWS_REGION ? require('./aws') : require('./memory');
